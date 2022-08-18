07631a9a6cdb14e8eef6cc82f29a83ea18d67a7b
olix3001 <olimioli9@gmail.com>
2022-08-18 22:54:08 +0200

    decrease amount of C# code to reasonable

--- src/Controllers/Parameters/FromRoute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Brass.Controllers.Parameters;
-
-[AttributeUsage(AttributeTargets.Parameter)]
-public class FromRoute : Attribute {}
